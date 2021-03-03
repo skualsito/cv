7d8b44de480f2f36b0a09aaa6532d46cc2bebb22
Juan Almada <skual@live.com>
2021-03-03 07:33:55 -0300

    new cositas

--- page/src/views/Porfolio.vue
+++ page/src/views/Porfolio.vue
@@ -1,38 +1,33 @@
 <template>
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+<div class="slide-profile">
+  <ol class="slide-indicators">
+    <li data-slide="0" class="active"></li>
+    <li data-slide="1"></li>
   </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-        <div class="carousel-caption d-none d-md-block">
+
+  <div class="slide-items">
+    <div class="slide-item active">
+        <div class="slide-title">
             <h5>JSmart</h5>
         </div>
-      <img class="d-block w-100" src="@/assets/slide/jsmart.png" alt="First slide">
-        <div class="carousel-caption d-none d-md-block">
+        <img class="d-block w-100" src="@/assets/slide/jsmart.png">
+        <div class="slide-desc">
             <p>Aplicación de Google Chrome para convertir el navegador en un SmartTV</p>
         </div>
     </div>
-    <div class="carousel-item">
-        <div class="carousel-caption d-none d-md-block">
+    <div class="slide-item">
+        <div class="slide-title">
             <h5>JSmart</h5>
         </div>
-      <img class="d-block w-100" src="@/assets/slide/jsmart.png" alt="First slide">
-        <div class="carousel-caption d-none d-md-block">
+        <img class="d-block w-100" src="@/assets/slide/jsmart.png">
+        <div class="slide-desc">
             <p>Aplicación de Google Chrome para convertir el navegador en un SmartTV</p>
         </div>
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+
 </div>
+
 </template>
 
 <script>
@@ -44,29 +39,49 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.carousel-item {
+.slide-item {
     background: rgba(0, 0, 0, 0.5);
 }
-.carousel-item img {
+.slide-item img {
     filter: blur(3px);
     mix-blend-mode: darken;
     height: 100vh;
 }
-.carousel-item h5 {
+.slide-title h5 {
     font-size: 16vw;
     text-align: left;
     letter-spacing: -17px;
     font-weight: 900;
     margin-bottom: 6vh;
 }
-.carousel-caption p {
+.slide-desc p {
     text-align: left;
     width: 30vw;
     font-size: 1.5vw;
     line-height: 1.7vw;
     font-weight: 200;
 }
-.carousel-control-prev, .carousel-control-next {
-  display: none;
+.slide-profile {position: relative;}
+
+.slide-items {position: relative;width: 100%;overflow: hidden;}
+
+.slide-indicators {position: absolute;right: 0;bottom: 0;left: 0;z-index: 2;display: flex;justify-content: center;padding: 0;margin-right: 15%;margin-bottom: 1rem;margin-left: 15%;list-style: none;}
+
+.slide-title, .slide-desc {position: absolute;right: 15%;bottom: 1.25rem;left: 15%;padding-top: 1.25rem;padding-bottom: 1.25rem;color: #fff;text-align: center;}
+
+.slide-item {
+    position: relative;
+    display: none;
+    float: left;
+    width: 100%;
+    margin-right: -100%;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    transition: transform 0.6s ease-in-out;
 }
+
+.slide-item.active {
+    display: block;
+}
+
 </style>
